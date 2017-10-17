7d5cdab60317b568d128a611dc16807b7a6f5752
Leo Loki <leo@LeonoMac-Pro.local>
2017-10-18 05:25:19 +0900

    ver.20171017c
    +++++ jQueryの調整 +++++

--- js/script.js
+++ js/script.js
@@ -1,48 +1,56 @@
 // jQuery Document
 
 $(function(){
-	/* block name setup */
+	/* Section ID setting */
 	var wrapId = "#contents";
-	var secId01 = "#sec01";
-	var secId02 = "#sec02";
-	var secId03 = "#sec03";
-	var secId04 = "#sec04";
-	var secId05 = "#sec05";
-	var naviId = "#mainMenu";
-	/* /block name setup */
-	var speed = 600; // smooth scroll speed
+	var secId = ["#sec01",
+				 "#sec02",
+				 "#sec03",
+				 "#sec04",
+				 "#sec05"];
+	/* /Section ID setting */
 
-	/* variable setup */
-	var boxWrap = $(wrapId),
-		sec01 = $(secId01), sec02 = $(secId02), sec03 = $(secId03), sec04 = $(secId04), sec05 = $(secId05),
-		navi = $(naviId),
-		win = $(window);
-	var winWidth = win.width(), winHeight = win.height(),
-		pos1 = sec01.offset().top, pos2 = sec02.offset().top, pos3 = sec03.offset().top, pos4 = sec04.offset().top, pos5 = sec05.offset().top;
+	/* other variable setting */
+	var navId = "mainMenu"; // navi block
+	var speed = 600; // smooth scroll speed
+	/* /other variable setting */
 	
-	/* load & resize */
-	function reload(){
-		winWidth = win.width(), winHeight = win.height(),
-			pos1 = sec01.offset().top, pos2 = sec02.offset().top, pos3 = sec03.offset().top, pos4 = sec04.offset().top, pos5 = sec05.offset().top;
+	var boxWrap = $(wrapId),
+		navi = $(navId),
+		win = $(window),
+		winWidth, winHeight;
+	var posArr = new Array();
+	/* position check */
+	function posCheck(posArr){
+		winWidth = win.width(), winHeight = win.height();
+		for(var i=0; i<secId.length; i++) {
+			posArr[i] = $(secId[i]).offset().top;
+		}
 		var offsetY = 0;
-		if(pos1<0){ offsetY = -pos1; }
-		pos1 += offsetY; pos2 += offsetY; pos3 += offsetY; pos4 += offsetY; pos5 += offsetY;
+		if(posArr[0]<0){ offsetY = -posArr[0]; }
+		for(var i=0; i<secId.length; i++) {
+			posArr[i] += offsetY;
+			/*console.log(secId[i] + " : " + posArr[i]);*/
+		}
+		return winWidth, winHeight, posArr;
     }
-	win.load(function(){ reload(); });
-	win.resize(function(){ reload(); });
-	/* /load & resize */
+	win.load(function(){ posCheck(posArr); });
+	win.resize(function(){ posCheck(posArr); });
+	/* /position check */
 
 	/* smooth scroll */
 	$('a[href^="#"]').click(function(){
 		var href = $(this).attr("href");
 		var target = $(href == "#" || href == "" ? 'html' : href);
 		var targetID = target.selector;
-		var position;
-		if(targetID == secId01){ position = pos1;
-		} else if(targetID == secId02){ position = pos2;
-		} else if(targetID == secId03){ position = pos3;
-		} else if(targetID == secId04){ position = pos4;
-		} else if(targetID == secId05){ position = pos5; } else { position = 0; }
+		var position = 0;
+		for(var i=0; i<secId.length; i++) {
+			if(targetID == secId[i]){
+				position = posArr[i];
+				/*console.log(targetID + " : " + position);*/
+				break;
+			}
+		}
 		boxWrap.animate({scrollTop:position}, speed, 'swing');
 		return false;
 	});
